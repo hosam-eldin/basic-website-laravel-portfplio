eeb7ece3678e75a499700c0714989540457aa778
hosam-eldin <h.eldinmoh95@gmail.com>
2025-08-30 11:50:59 +0300

    frontend tamplates

---
+++ resources/views/frontend/body/footer.blade.php
@@ -0,0 +1,37 @@
+<footer>
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-4 col-md-6">
+            <div class="footer__widget">
+               <h4 class="footer__title">About Us</h4>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet accumsan arcu. Sed vitae
+                  nisi
+                  malesuada, tincidunt nunc vel, tincidunt nunc.</p>
+               <a href="about.html" class="btn">Read More</a>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="footer__widget">
+               <h4 class="footer__title">Quick Links</h4>
+               <ul class="footer__links">
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="about.html">About</a></li>
+                  <li><a href="services.html">Services</a></li>
+                  <li><a href="portfolio.html">Portfolio</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+               </ul>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="footer__widget">
+               <h4 class="footer__title">Contact Us</h4>
+               <ul class="footer__contact">
+                  <li><i class="fas fa-map-marker-alt"></i> 123 Street, City, Country</li>
+                  <li><i class="fas fa-phone"></i> +123 456 7890</li>
+                  <li><i class="fas fa-envelope"></i> info@example.com</li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
